14d9f1fc9c329dc66810d08cb1e7334b49e2e222
Andika Heru P <andikaherup@gmail.com>
2025-06-15 14:23:21 +0700

    feat: initialize Ionic Vue project structure

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'transaction-history-frontend',
+  webDir: 'dist'
+};
+
+export default config;
